d9b0531ff65394ed4dadfad8d818140c86156d52
FYS <fysphilip@gmail.com>
2022-08-11 13:06:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import "./static/App.css";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import InfoBox from "./components/InfoBox";
+import Map from "./components/Map";
+import Table from "./components/Table";
+import { sortData, prettyPrintStat } from "./util";
+import LineG from "./components/LineG";
+import numeral from "numeral";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const Taiwan = {
+    lat: 23.5,
+    lng: 121,
+  };
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState(["worldwide"]);
+  const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState(Taiwan);
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+
+  // Worldwide API call
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  // Each Country API call
+  // useEffect = runs a piece of code based on a given condition
+  useEffect(() => {
+    // code inside here will run once when the component loads and not again
+    // async -> send a request, wait for it, do something with info
+    const getCountriesData = async (event) => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        // loop throgh all the countries and assign to name and value
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setMapCountries(data);
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []); // when condition change, useEffet will excute the code again
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    // output: the value=""  of MenuItem
+    setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+        console.log(data.countryInfo);
+        if (countryCode === "worldwide") {
+          setMapCenter(Taiwan);
+        } else {
+          setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        }
+        setMapZoom(4);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__left">
+        {/* Header */}
+        <div className="app__header">
+          <h1>COVID-19 Tracker App</h1>
+
+          {/* Title + Select input dropdown field */}
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {/* Loop through all the countries and show a dropdown list of the option */}
+              {countries.map((country) => {
+                return (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          {/* InfoBoxs title="Covid cases" */}
+          <InfoBox
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
+            isRed
+            active={casesType === "cases"}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format("0.0a")}
+          />
+          {/* InfoBoxs Deaths*/}
+          <InfoBox
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            isRed
+            active={casesType === "deaths"}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
+          />
+        </div>
+        {/* Map */}
+        <Map
+          casesType={casesType}
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases By Country</h3>
+          {/* Table */}
+          <Table countries={tableData} />
+        </CardContent>
+      </Card>
+      <div className="app__bottom">{/* <LineG casesType={casesType} /> */}</div>
     </div>
   );
 }
